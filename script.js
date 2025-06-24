2e846b59451caf6ed6f8fade70cc4f395cabc217
NaNeLou <126290968+Nanelou@users.noreply.github.com>
2025-06-24 17:18:57 -0300

    24jun

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function inserirNome (){
+function inserirNome(){
     let nomeUsuario = prompt("Qual é o seu nome?");
     let elemento = document.querySelector ("#nome-ususario");
     elemento.textContent = nomeUsuario;
 }
 
-inserirNome ():
+inserirNome();
 
